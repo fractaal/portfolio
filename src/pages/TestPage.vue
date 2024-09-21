34678f0a6bf1f070a986ff156c6cb79c4e5180a7
Ben <sonoroustwo@gmail.com>
2024-09-21 22:14:01 +0800

    Hidden test page

---
+++ src/pages/TestPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page>
+    <Suspense>
+      <FancyWaves :speed="1" :noise-strength="1" />
+    </Suspense>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import FancyWaves from 'src/components/FancyWaves.vue';
+</script>
